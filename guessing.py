a8aa783617617412d18fcc8e2b39824fee623b34
reck0014 <adrianoreck@gmail.com>
2021-01-13 17:43:55 -0500

    Test 1

---
+++ guessing.py
@@ -0,0 +1,49 @@
+import random
+
+print("*****************************")
+print("Welcome to the guessing game!")
+print("*****************************")
+
+chances = 0
+
+while chances == 0:
+    print("Choose level:\n(1) easy\n(2) intermediate\n(3) hard")
+    level = int(input("Hit 1, 2 or 3:"))
+
+    if level == 1:
+        chances = 15
+    elif level == 2:
+        chances = 10
+    elif level == 3:
+        chances = 5
+    else:
+        print("MUST be 1, 2 or 3. Try again:")
+
+secret_number = random.randrange(1, 101)
+
+for guesses in range(0, chances):
+
+    print("You have {} tries".format(chances))
+    guess = int(input("Insert your number (1-100): "))
+    print("You entered ", guess)
+
+    chances = chances - 1
+
+    if guess < 1 or guess > 100:
+        print('MUST be 1-100!')
+        continue
+
+    hit = guess == secret_number
+    high = guess > secret_number
+    low = guess < secret_number
+
+    if hit:
+        print("Gotcha! That's the answer of the Universe!")
+        break
+    else:
+        if high:
+            print("You missed! Too high.")
+        elif low:
+            print("You missed! Too low.")
+
+print('Game Over, and thank you for the fish')
